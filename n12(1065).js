27706a1e4fc3d71fa9985802c5ea0009e82a1ab1
TaeTaehoon <75843118+TaeTaehoon@users.noreply.github.com>
2021-08-18 22:47:13 +0900

    Solve!!!!

--- n12(1065).js
+++ n12(1065).js
@@ -1,34 +1,41 @@
 let input = require('fs').readFileSync('input.txt').toString().split('\n');
 
 let result = [];
-//result에 빈 배열을 생성하여 여기에 결과값들을 저장
 
-for (n = 1; n <= Number(input[0]); n++) {
-    let number = n;
-    let g = [];
-
-    for (i = 0; i < String(n).length; i++) {
-        g.push(number % 10);
-        number = Math.floor(number / 10);
-    }
-    //input을 자리수마다 잘라서 배열로 순서대로 저장
-
-    if (n < 100) {
-
-        result.push(n);
-
-    } else if (100 <= n && n <1000) {
-
-        if (g[2] - g[1] == g[1] - g[0]) {
-            result.push(n);
+function filter(n) {
+        let number = n;
+        let g = [];
+    
+        for (j = 0; j < String(n).length; j++) {
+            g.push(number % 10);
+            number = Math.floor(number / 10);
         }
-
-    } else if ( n == 1000) {
-        break;
+        //input을 자리수마다 잘라서 배열로 순서대로 저장
+    
+        if (n < 100) {
+    
+            return result.push(n);
+    
+        } else if (100 <= n && n <1000) {
+    
+            if (g[2] - g[1] == g[1] - g[0]) {
+                return result.push(n);
+            }
+    
+        } else {
+            return;
+        }
+        //1~99는 항상 한수이기때문에 그 이상을 따로 나누어 한수검사 실행!
+        //이걸 좀 줄이고 싶은데 좀처럼 떠오르지 않는다..언젠간 바꾸리..!
     }
-    //1~99는 항상 한수이기때문에 그 이상을 따로 나누어 한수검사 실행!
-    //이걸 좀 줄이고 싶은데 좀처럼 떠오르지 않는다..언젠간 바꾸리..!
-}
+    //초기의 반복문을 함수형으로 바꿔서 사용하는데 성공했다!
+    //함수탈출은 break가 아니라 return임! 거기에서 옆에 뭐 붙이면 그걸 뿅하고 도출!
 
-console.log(result.length);
-//result에 저장된 결과값의 개수를 출력
+
+
+    
+    for(i = 1; i <= Number(input[0]); i++) {
+        filter(i);
+    }
+
+    console.log(result.length);
