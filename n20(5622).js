9c5a02a24fabc796b55242298ae4f960bf0291a3
TaeTaehoon <75843118+TaeTaehoon@users.noreply.github.com>
2021-08-27 23:19:17 +0900

    solve!!!!!!!!!!!!!!!!!!

--- n20(5622).js
+++ n20(5622).js
@@ -1,7 +1,12 @@
 const input = require('fs').readFileSync('input.txt').toString().split('');
+input.pop();
+//백준에서 input을 읽어오는 과정에서 뭔가 문제가 생겼던것 같다.
+//내 텍스트 편집기 상으로는 아무문제가 없었는데...어이가 없다!
+//아무래도 split메소드의 특성상 뭐 줄바꿈 문자가 들어갔다거나,,?
+//다음부터는 trim메소드를 사용해봐야겠다. 그게 오류가 없단다!
 
 const string = ['A', 'B', 'C', 'D', 'E','F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-                 'N', 'O', 'P', 'Q', 'R', 'S', 'T','U', 'V', 'W', 'X', 'Y', 'Z'];
+                 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 let result = 0;
 
 
@@ -11,25 +16,21 @@ for( i = 0; i <= input.length - 1; i++) {
 
     if (num <= 2) {
         result += 3;
-    } else  if (num <= 5 && 2 < num) {
+    } else  if (num <= 5) {
         result += 4;
-    } else  if (num <= 8 && 5 < num) {
+    } else  if (num <= 8) {
         result += 5;
-    }  else  if (num <= 11 && 8 < num) {
+    }  else  if (num <= 11) {
         result += 6;
-    }  else  if (num <= 14 && 11 < num) {
+    }  else  if (num <= 14) {
         result += 7;
-    } else  if (num <= 18 && 14 < num) {
+    } else  if (num <= 18) {
         result += 8;
-    }  else  if (num <= 21 && 18 < num) {
+    }  else  if (num <= 21) {
         result += 9;
-    }  else  if (num <= 25 && 21 < num) {
+    }  else  if (num <= 25) {
         result += 10;
     }
 
 }
-//type 문제라고 생각해서 Number태그를 이용하였는데 여전히 풀리지않는다...
-
-console.log(input);
-console.log(string.length);
 console.log(result);
