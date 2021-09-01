1bb64c0b6098d1407c0c03d9175c72a572e56450
TaeTaehoon <75843118+TaeTaehoon@users.noreply.github.com>
2021-09-01 23:06:30 +0900

    solve!

--- n25(1192).js
+++ n25(1192).js
@@ -2,20 +2,30 @@ let input = require('fs').readFileSync('input.txt').toString().split(' ');
 
 
 let range = 1, block = 1;
+let line = [];
+
 
 while(block < input) {
-    block += 1 + range;
-
     range++;
+
+    block += range;
 }
 
-console.log(range);
-console.log(block);
+if (Math.floor(range % 2) == 0) {
 
-if ( Math.floor(range % 2) == 0) {
+    for (i = 0; i <= range - 1; i++) {
+        line.push(`${i + 1}/${range - i}`);
+    }
 
-}
+    console.log(input[0] == 2 ? '1/2' : line[Number(input[0]) - (block - range + 1)]);
+}//짝수 행에 대해
 else if (Math.floor(range % 2) != 0) {
 
-}
-//block 내의 숫자의 순서를 찾는다 => 해당 위치에 알맞는 분수값을 지정하는 일이 남음!
+    for (i = 0; i <= range - 1; i++) {
+        line.push(`${range - i}/${i + 1}`);
+    }
+
+    console.log(range == 1 ? '1/1 ': line[Number(input[0]) - (block - range + 1)]);
+}//홀수 행에 대해!
+//확실히 삼행연산자를 쓰니까 예외사항들을 표기하기가 편하다!
+//처음에 문자 순서를 배열마다 쪼개서 생각하다보니 힘들었는데 예외사항 몇개 빼니까 금방 했다! 유후~
