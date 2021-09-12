56fa52840dba79b3410d788070fed25d1b45c466
SSC <75843118+TaeTaehoon@users.noreply.github.com>
2021-09-12 23:56:37 +0900

    wtf...

--- n31(1011).js
+++ n31(1011).js
@@ -2,69 +2,30 @@ let input = require('fs').readFileSync('input.txt').toString().trim().split('\n'
 
 const T = Number(input[0]);
 
-function solve(n) {
-    const coordinate = input[n].split(' ');
+function solve(t) {
+    const coordinate = input[t].split(' ');
     let x = Number(coordinate[0]);
     let y = Number(coordinate[1]);
-    const arrive = y-1;
-    let distance = arrive-x;
-    let jump = 1;
-    
+    let distance = y-x;
+
+    let n = 0;
+
     while(true) {
 
-        if(distance > 0) {
-            distance -= jump;
-            jump++;
-        } else
-        if(distance == 0) {
-            return jump+1;
-            break;
-        } else
-        if(distance < 0) {
-            jump--;
+        if(distance <= n*(n+1)) {
+            if(distance <= n^2) {
+                return (n*2)-1;
+            } else 
+            if(distance > n^2) {
+                return n*2;
+            }
         }
-
+        
+        n++;
     }
+    
 }
 
 for(i = 1; i <= T; i++) {
     console.log(solve(i));
 }
-/*
-1
-1
-
-2
-1 1
-
-3
-1 1 1
-
-4
-1 1 1 1
-1 2 1
-
-5
-1 1 1 1 1
-1 2 1 1
-
-6
-1 1 1 1 1 1
-1 2 2 1
-
-7
-1 1 1 1 1 1 1
-1 2 2 1 1
-
-8
-1 1 1 1 1 1 1 1
-1 2 2 2 1
-
-9
-1 1 1 1 1 1 1 1 1
-1 2 3 2 1
-
-10
-1 1 1 1 1 1 1 1 1 1
-1 2 3 2 1 1
-*/
