2132a0cc9862f037572e4f9d14d44bd9ad34e77d
SSC <75843118+TaeTaehoon@users.noreply.github.com>
2021-09-13 09:47:19 +0900

    solve!
    놀랍게도 n^2를 인식하지 못해서 자꾸 답이 안나오더라...제곱을 어떻게 표현하는건지 다시 확인해야겠음...

--- n31(1011).js
+++ n31(1011).js
@@ -3,27 +3,20 @@ let input = require('fs').readFileSync('input.txt').toString().trim().split('\n'
 const T = Number(input[0]);
 
 function solve(t) {
-    const coordinate = input[t].split(' ');
+    const coordinate = input[t].toString().split(' ');
     let x = Number(coordinate[0]);
     let y = Number(coordinate[1]);
-    let distance = y-x;
+    const distance = (y-x);
 
     let n = 0;
 
     while(true) {
-
         if(distance <= n*(n+1)) {
-            if(distance <= n^2) {
-                return (n*2)-1;
-            } else 
-            if(distance > n^2) {
-                return n*2;
-            }
+            return (distance > n*n ? n*2 : n*2-1);
+        }else {
+            n++;
         }
-        
-        n++;
     }
-    
 }
 
 for(i = 1; i <= T; i++) {
